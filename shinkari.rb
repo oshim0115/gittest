011aaa1c57fa4ff1cf807ded35ef35570b29e637
yoshitakamimura <kix11974@yahoo.co.jp>
2020-05-29 16:27:11 +0900

    initial commit

---
+++ shinkari.rb
@@ -0,0 +1,46 @@
+# x = 0
+# i = 0
+# j = 0
+
+# 2.times do
+#   j = 0
+# puts j
+#   4.times do
+#     x += 1
+#     j += 1
+#   end
+# puts j
+#  x += 1
+#  i += 1
+# end
+
+
+# array = [{ name: "Yoshida", speed: 150 }, { name: "Matsumoto", speed: 147
+# }, ["speed", 136]]
+# puts array[2][1]
+
+# colors = ["あか", "あお", "きいろ"]
+# element_count = colors.length
+
+# element_count.times do |i|
+#    i = i + 1
+# end
+
+# puts i
+
+# posts = ["あか", "みどり", "黄色", "オレンジ", "黒"]
+
+#  num = 0
+
+# posts.each do |post|
+#   num = num + 1
+# end 
+
+# puts num
+animals = ["いぬ", "ねこ", "ねずみ"]
+num = 0
+animals.each do |animal|
+  number = number + 1
+end
+number = 5
+  
